88e2201a4a7edadd5811a7f3c9467d5421e05682
Alfred Dai <3542675947@qq.com>
2022-09-05 19:53:20 +0800

    Update Station.cs

--- EDCHost24/Station.cs
+++ EDCHost24/Station.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace EDCHOST24
 {
     class Station //所有站点
     {
-        public int num;//站点数量
-        public list<Dot> mStationList; //一个包含站点位置信息的list
-        public void ResetIndex() 
+        private int MAX_STATION = 3;
+        private List<Dot> mStationList; //一个包含站点位置信息的list
+        public void Reset() 
         { 
-            num = 0; 
             mStantionList.Clear();
         } //num复位
-        public Station(int Num = 0) //构造函数
+        public Station() //构造函数
         { 
-            num = Num;
             list<Dot> mStationList = new list<Dot>();
         }
-        public void Add(Car _car)
+        public void Add(Dot _inPos)
+        {
+            if (mStationList.Count() < MAX_STATION)
+            {
+                if (_isNewStationLegal(_inPos))
+                {
+                    mStationList.Add(_inPos);
+                    Debug.WriteLine("New Station, ({0}, {1})", _inPos.x, _inPos.y);
+                }
+                else
+                {
+                    Debug.WriteLine("Failed! Location is illegal");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Failed! Up to maximum");
+            }
+        }
+
+        private bool _isNewStationLegal(Dot _inPos)
         {
 
         }
