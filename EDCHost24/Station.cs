2dbfa3c7f88e706dddebe99f2228ddba80b87b66
Alfred Dai <3542675947@qq.com>
2022-09-05 20:21:32 +0800

    Update

--- EDCHost24/Station.cs
+++ EDCHost24/Station.cs
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -58,72 +57,3 @@ namespace EDCHOST24
     }
 
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
-
-namespace EDCHOST24
-{
-    public class Station //所有站点
-    {
-        private const int MAX_STATION = 3;
-        private List<Dot> mStationList; //一个包含站点位置信息的list
-        public void Reset() 
-        { 
-            mStantionList.Clear();
-        }
-        public Station() //构造函数
-        { 
-            List<Dot> mStationList = new List<Dot>();
-        }
-
-        public void AddStation (Dot _new_station)
-        {
-            if (mStationList.Count() < MAX_STATION)
-            {
-                if (_isPosLegal(_new_station))
-                {
-                    mStationList.Add(_new_station);
-                    Debug.WriteLine("New station has been set, ({0}, {1})", _new_station.x, _new_station.y);
-                }
-                else
-                {
-                    Debug.WriteLine("Failed! The postion of new station is illegal");
-                }
-            }
-            else
-            {
-                Debug.WriteLine("Failed! The number of charging stations has reached the upper limit");
-            }
-
-        }
-
-        public static bool isCollided(Dot CarPos, int radius)
-        {
-            foreach(Dot dot in mStationList)
-            {
-                if (Utility.DistanceP(CarPos, dot) < radius)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public int Count()
-        {
-            return mStationList.Count();
-        }
-
-        private static bool _isPosLegal (Dot _inDot)
-        {
-
-        }
-    }
-
-}
->>>>>>> Stashed changes
